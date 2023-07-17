4a687815802e18d74cc68fce28644d7b37046f35
DavideCruciani <cruciani240203@gmail.com>
2023-07-17 14:41:47 +0200

    hero section and navbar done (no responsive)

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = ({ immagine }) => {
       <div className='flex'>
         <div>
           <h1 className='font-typewriter text-9xl'>Frontend Developer</h1>
-          <p className='font-typewriter text-xl mt-8'>Creatività e codice si fondono per creare esperienze <br /> digitali coinvolgenti e innovative.</p>
+          <p className='font-typewriter text-xl mt-8'>Creativita' e codice si fondono per creare esperienze <br /> digitali coinvolgenti e innovative.</p>
           <button className='font-typewriter p-4 border-black border-[3px] mt-12 hover:bg-black hover:text-white'>Mettiamoci in contatto</button>
         </div>
         <div>
