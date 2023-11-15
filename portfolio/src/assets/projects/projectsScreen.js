c426866c7bc5ec6da80e2a73bcb287b2ac9e30b4
DavideCruciani <cruciani240203@gmail.com>
2023-11-15 23:31:59 +0100

    new react native project added

--- portfolio/src/assets/projects/projectsScreen.js
+++ portfolio/src/assets/projects/projectsScreen.js
@@ -1,6 +1,7 @@
-import Project1 from '../projects/geolocator.png'
-import Project2 from '../projects/Homepage-BMW.jpg'
-import Project3 from '../projects/HooBank.png'
-import Project4 from '../projects/Space-tourism.jpg'
+import Project1 from "../projects/geolocator.png";
+import Project2 from "../projects/Homepage-BMW.jpg";
+import Project3 from "../projects/HooBank.png";
+import Project4 from "../projects/Space-tourism.jpg";
+import Project5 from "../projects/RestaurantSearch.png";
 
-export { Project1, Project2, Project3, Project4}
+export { Project1, Project2, Project3, Project4, Project5 };
